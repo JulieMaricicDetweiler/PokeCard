b0161ea35a272e17e42919ed18537ca5ce393302
JulieMaricicDetweiler <80288445+JulieMaricicDetweiler@users.noreply.github.com>
2022-04-18 16:46:20 -0400

    Add files via upload

--- DataScraper.py
+++ DataScraper.py
@@ -4,29 +4,30 @@ from time import sleep
 import requests
 import csv
 
-header = ['Name', 'Rarity', 'Price']
+header = ['Name', 'Rarity', 'Price', 'Set']
 
 html_text = requests.get('https://shop.tcgplayer.com/price-guide/pokemon/swsh07-evolving-skies').text
 soup = BeautifulSoup(html_text, 'lxml')
 select = soup.find_all('select', class_='priceGuideDropDown')
 options = select[1].find_all('option')
 for option in options:
+    card_set = option.text.strip()
     url = 'https://shop.tcgplayer.com/price-guide/pokemon/' + option['value']
     print(url)
+    print(card_set)
     page = requests.get(url).text
     soop = BeautifulSoup(page, 'lxml')
     cards = soop.find_all('tr', class_='even') + soop.find_all('tr', class_='odd')
 
     with open('cards.csv', 'a') as f:
-        writer = csv.writer(f)
+        writer = csv.writer(f, delimiter=',')
         writer.writerow(header)
 
         for card in cards:
-            name = card.find('div', class_='productDetail').text.replace('\n', '')
-            price = card.find('td', class_='marketPrice').find('div', class_='cellWrapper').text\
-                .replace(' ', '').replace('\n', '')
-            rarity = card.find('td', class_='rarity').find('div', class_='cellWrapper').text\
-                .replace('  ', '').replace('\n', '')
-            data = [name, rarity, price]
+            name = card.find('div', class_='productDetail').text.strip("\n").strip()
+            price = card.find('td', class_='marketPrice').find('div', class_='cellWrapper').text.strip("\n").strip()
+            rarity = card.find('td', class_='rarity').find('div', class_='cellWrapper').text.strip("\n").strip()
+            data = [name, rarity, price, card_set]
             writer.writerow(data)
-    # sleep(randint(2, 6))
+
+        sleep(randint(0, 2))
